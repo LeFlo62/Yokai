5fc869dcf19385ed6b4ce278c31d0561c6b98491
LeFlo <leflodu62@live.fr>
2021-12-04 17:53:16 +0100

    Updated PauseLayer

--- src/fr/qmf/yokai/game/gui/layers/PauseLayer.java
+++ src/fr/qmf/yokai/game/gui/layers/PauseLayer.java
@@ -2,18 +2,23 @@ package fr.qmf.yokai.game.gui.layers;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.image.BufferedImage;
 
+import fr.qmf.yokai.io.Textures;
 import fr.qmf.yokai.ui.UILayer;
 import fr.qmf.yokai.ui.Window;
+import fr.qmf.yokai.ui.components.ImageComponent;
 import fr.qmf.yokai.ui.components.TextComponent;
 
 public class PauseLayer extends UILayer {
 
+	private BufferedImage pause;
 	private static final Color BACKGROUND_COLOR = new Color(20, 20, 20, 220);
 	
 	public PauseLayer(Window window) {
 		super(window, 0, 0, Window.WIDTH, Window.HEIGHT);
-		add(new TextComponent(this, "Pause", TextComponent.DEFAULT_FONT, Color.WHITE, 100, 100));
+		pause = Textures.getTexture("gui/pause");
+		add(new ImageComponent(this, pause, (Window.WIDTH - pause.getWidth())/2, 100));
 	}
 	
 	@Override
