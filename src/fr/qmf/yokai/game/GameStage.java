cc9e7c28d0fcf3f64b26f4212e46cb3d77ad9668
LeFlo <leflodu62@live.fr>
2021-12-17 11:19:21 +0100

    Added description to GameStage

--- src/fr/qmf/yokai/game/GameStage.java
+++ src/fr/qmf/yokai/game/GameStage.java
@@ -2,15 +2,21 @@ package fr.qmf.yokai.game;
 
 public enum GameStage {
 	
-	PLAY_OR_GUESS(0),
-	OBSERVING(1),
-	MOVING(2),
-	HINT(3);
+	PLAY_OR_GUESS(0, "Observez des Yokai ou déclarez qu'ils sont appais�s"),
+	OBSERVING(1, "Observez un Yokai"),
+	MOVING(2, "Déplacez un Yokai"),
+	HINT(3, "Préparez un indice ou utilisez-en un");
 	
 	private int number;
+	private String description;
 	
-	private GameStage(int number) {
+	private GameStage(int number, String description) {
 		this.number = number;
+		this.description = description;
+	}
+	
+	public String getDescription() {
+		return description;
 	}
 	
 	public GameStage getNextStage() {
