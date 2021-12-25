067a8ae1b32fa090e9a276bc1c52501da131fee2
LeFlo <leflodu62@live.fr>
2021-12-25 14:29:30 +0100

    Update HintsLayer.java

--- src/fr/qmf/yokai/game/gui/layers/HintsLayer.java
+++ src/fr/qmf/yokai/game/gui/layers/HintsLayer.java
@@ -3,6 +3,7 @@ package fr.qmf.yokai.game.gui.layers;
 import java.awt.Graphics2D;
 
 import fr.qmf.yokai.YokaiGame;
+import fr.qmf.yokai.game.GameStorage;
 import fr.qmf.yokai.ui.UILayer;
 import fr.qmf.yokai.ui.Window;
 
@@ -13,11 +14,11 @@ public class HintsLayer extends UILayer {
 	public HintsLayer(YokaiGame game, Window window, UILayer parent) {
 		super(window, parent, 0,0,0,0);
 		this.game = game;
-		YokaiType.get
 	}
 	
 	@Override
 	public void draw(Graphics2D g) {
+		GameStorage storage = game.getGameStorage();
 		
 	}
 
